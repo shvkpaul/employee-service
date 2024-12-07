38595674f262d1ee65ca1d6d78d5d9ddeeefd36d
Shouvik Paul <shouvik.paul@ah.nl>
2024-12-07 20:16:29 +0100

    initial commit

---
+++ src/main/java/com/shvkpaul/employeeservice/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.shvkpaul.employeeservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "Test Successful";
+    }
+}
